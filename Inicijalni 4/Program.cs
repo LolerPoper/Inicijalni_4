7f2f28600e41b469650c2a7f26a789271c2776c3
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-09-11 10:57:05 +0200

    Add files via upload

---
+++ Inicijalni 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.WriteLine("Unesite vrijednosti za x, y i z stranice pravokutnog trokuta s tim da je z hipotenuza: ");
+            do
+            {
+                x = Convert.ToInt16(Console.ReadLine());
+            }
+            while (x <= 0);
+            do
+            {
+                y = Convert.ToInt16(Console.ReadLine());
+            }
+            while (y <= 0);
+            do
+            {
+                z = Convert.ToInt16(Console.ReadLine());
+            }
+            while (z <= 0);
+
+            if(z == Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
+            {
+                Console.WriteLine("Ovo su strane pravokutnog trokuta.");
+            }
+            else
+            {
+                Console.WriteLine("Ovo nisu strane pravokutnog trokuta.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
